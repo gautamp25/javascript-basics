9eb02f0d0c3a8d0dbdcb5f619db107c13363f52b
gautamp25 <45966893+gautamp25@users.noreply.github.com>
2020-01-19 01:17:03 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+// let const
+let score= 30
+score=48
+console.log(score)
+
+const num=12
+// num=12 //Error
+
+// string Numbers Boolean null undefined
+const name='Gautam'
+const age=30
+const rating=4.5
+const isCool=true
+const a=null
+const b=undefined
+let c
+
+console.log(typeof z)
+console.log(name.length)
+console.log(name.substring(0,4).toUpperCase())
+console.log(name.split(''))
+
+// Template string
+console.log(`My name is ${name} and I am ${age}`)
+
+
+
